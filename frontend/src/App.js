3717b7357b939efef303c85705e71739ee39daf6
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2024-12-13 11:44:23 +0530

    stripe2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './CheckoutForm';
-
-const stripePromise = loadStripe('pk_test_51QVHsVDwN2wl0CGWHVNfAf55kmipeSlvyGeMz7bZ73HtkZI6cWaYTrMe2efChawehSE3cEyDUI9o0sbxyjNyaD2m00vLlarLtl'); // Publishable key
 
 function App() {
   return (
     <div className="App">
-     <Elements stripe={stripePromise}>
-      <CheckoutForm />
-    </Elements>
+     
     </div>
   );
 }
